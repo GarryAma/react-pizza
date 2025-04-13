4955ef84f779caae330f1795686fe8fe23df7faf
Garry Amasia <amasiagarry23@gmail.com>
2025-04-13 15:15:01 +1000

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+
+export default [
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect", // ⬅️ penting
+        runtime: "automatic", // ⬅️ ini biar gak perlu import React
+      },
+    },
+    plugins: {
+      js,
+      react,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      "no-unused-vars": ["warn"],
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
