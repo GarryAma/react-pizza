ac6c199a143df77c8307c36e6ba7bac05d1ec8c6
Garry Amasia <amasiagarry23@gmail.com>
2025-05-08 22:09:14 +1000

    modeling user state with rtk as well as reading and updating user state

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Username = () => {
-  return <div className="text-sm font-semibold hidden md:block">Garry</div>;
+  const { username } = useSelector((state) => state.user);
+
+  return (
+    <div className="text-sm font-semibold hidden md:block">{username}</div>
+  );
 };
 
 export default Username;
